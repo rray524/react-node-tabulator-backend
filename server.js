228c5f33199c64ffb933bb9f04c47efaf57c7fdb
rray524 <rray524@gmail.com>
2022-11-16 22:22:06 +0600

    basic backend server and database setup

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const connectDb = require('./config/db');
+
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT
+
+app.use(express.json())
+
+
+// mongodb connection
+connectDb();
+
+app.get('/', (req, res) => {
+    res.json({ message: "Api connection has been established" })
+})
+
+// app.use('/api', apiRoutes);
+
+//console error 
+app.use((error, req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        console.error(error);
+    }
+
+    next(error);
+})
+
+// custom error handler
+app.use((error, req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        res.status(500).json({
+            message: error.message,
+            stack: error.stack
+        })
+    }
+
+    else {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Your app listening on port ${port}`)
+})
